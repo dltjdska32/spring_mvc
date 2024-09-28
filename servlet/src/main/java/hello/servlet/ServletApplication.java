8ad158839e7ef57be5e5ef9e1887e6276c97f542
dltjdska32 <dltjdska32@naver.com>
2024-09-28 19:23:53 +0900

    스프링mvc 구조 확인

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan//서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,14 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+
+
+
+	//아래의 viewResolver 설정을  application.properties 해당 파일에서 설정했음.
+	/*@Bean
+	InternalResourceViewResolver viewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}*/
 
 }
